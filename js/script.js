0cdb4f061504e01e37c1324a602c1b0ae752807b
Mx. Valix <vlipari@protonmail.com>
2020-05-27 14:37:40 +0200

    bordo

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
 let contatore = 0;
+let n=-1;
+let colori = ["#ffb3ba","#ffdfba","#ffffba","#baffc9","#bae1ff","#b7ded2","#f6a6b2","#f7c297","#ffecb8", "#90d2d8"];
 $(document).ready(function(){
     $("#plus-circle-btn").click(function(event){
         event.preventDefault();
         contatore++;
-        let n = 0;
-        let colori = ["#ffffcc","#e3e3e3"];
-        if (contatore%2==0) n = 0
-        else n=1;
+        if (n==9) n=0;
+        else n++;
         let aggiungiDiv = 
-            `<div class="col-sm-4 id="col-${contatore}" style="background-color:${colori[n]}">
-            <textarea class="txtarea mt-3" id="txt-${contatore}" style="background-color:${colori[n]}"></textarea>
-            <button type="button" id="salva-${contatore}" class="btn btn-sm bg-success text-white text-uppercase">Salva nota</button>
-            </div>`;
+            `   
+                <div class="col-sm-4 id="col-${contatore}" 
+                    style="background-color:${colori[n]}; border:3px solid white;   border-collapse: collapse">
+                    <textarea class="txtarea mt-3" id="txt-${contatore}" 
+                        style="background-color:${colori[n]}"></textarea>
+                    <button type="button" id="salva-${contatore}" 
+                        class="btn btn-sm bg-success text-white text-uppercase">
+                            Salva nota
+                    </button>
+                </div>
+            `;
         $("#aggiungi-qui").append(aggiungiDiv);      
         let txtArea = "#txt-" + contatore;
         $(txtArea).focus();  
     })
 
-    $()
+
 })
